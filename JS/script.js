54dbc04d978f305e0c3a34d77417e2753b311a4e
anosha25 <124650395+anosha25@users.noreply.github.com>
2023-03-07 18:13:36 +0000

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,3 @@
-const button = document.querySelector('.open-close');
-const menu = document.querySelector('#side-menu');
-
-button.addEventListener('click', function() {
-  menu.classList.toggle('active');
-});
+$('.open-close').click(function(){
+  $("#side-menu").toggleClass("active");
+})
